735dd9e2e9f8d5502e4ced3e65fe52a4c0355bf9
Tom Williams <77905483+willtonian1@users.noreply.github.com>
2023-01-09 22:01:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 //data_place = document.getElementById('data1')
+
+//getting elements from HTML
 points_place = document.getElementById("points_insert")
 rebounds_place = document.getElementById("rebounds_insert")
 
+//Request!
 axios.get('https://giannisapi.193ftgw.repl.co/stats').then(resp => {
 	data = resp.data;
 	//console.log(data)
 
 
-
+	//Place Data into HTML
 	//data_place.innerHTML = data;
 	points_place.innerHTML = data[22]
 	rebounds_place.innerHTML = data[16]
